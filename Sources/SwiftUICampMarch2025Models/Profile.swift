afe4b6354c11fcb3460fb7b70e7df23323c83816
alexatgetcalvinapp <alex@getcalvin.app>
2025-03-12 16:26:40 +0200

    public

--- Sources/SwiftUICampMarch2025Models/Profile.swift
+++ Sources/SwiftUICampMarch2025Models/Profile.swift
@@ -8,33 +8,33 @@
 import SwiftUI
 import FirebaseFirestore
 
-struct Profile: Codable, Identifiable {
-    @DocumentID var id: String?
+public struct Profile: Codable, Identifiable {
+    @DocumentID public var id: String?
     
-    var name: String
-    var score: Int
+    public var name: String
+    public var score: Int
     
-    init(name: String = "", score: Int = 0) {
+    public init(name: String = "", score: Int = 0) {
         self.name = name
         self.score = score
     }
     
-    var uid: String { id ?? "" }
+    public var uid: String { id ?? "" }
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case id
         case name
         case score
     }
     
-    init(from decoder: any Decoder) throws {
+    public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self._id = try container.decode(DocumentID<String>.self, forKey: .id)
         self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
         self.score = try container.decodeIfPresent(Int.self, forKey: .score) ?? 0
     }
     
-    func encode(to encoder: any Encoder) throws {
+    public func encode(to encoder: any Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encodeIfPresent(self.name, forKey: .name)
         try container.encodeIfPresent(self.score, forKey: .score)
